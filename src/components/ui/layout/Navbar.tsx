50b6f319f2981a1b03e67b2cb1a9dad08f53bfda
Andy Ansong <andyansong82@gmail.com>
2025-03-27 02:59:58 +0000

    feat add navbar

--- src/components/ui/layout/Navbar.tsx
+++ src/components/ui/layout/Navbar.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Navbar: React.FC = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex items-center p-5 shadow-sm justify-between w-full'>
+        <h1 className='text-2xl'>FlashTeXt⚡</h1>
+        <ul className='text-slate-700 flex gap-5'>
+            <li className='hover:text-black transition-colors'>Pricing</li>
+            <li className='hover:text-black transition-colors'>Log in</li>
+            <li className='hover:text-black transition-colors'>Sign up</li>
+        </ul>
+    </nav>
   )
 }
 
